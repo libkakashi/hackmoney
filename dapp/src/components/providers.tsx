f4ec22fb5c78605511c89a4e66bba02b804fe598
Apoorv <github@libkakashi.dev>
2026-01-31 19:36:17 +0530

    add themes and shadcn

--- dapp/src/components/providers.tsx
+++ dapp/src/components/providers.tsx
@@ -4,6 +4,7 @@ import {useState} from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {WagmiProvider} from 'wagmi';
 import {RainbowKitProvider} from '@rainbow-me/rainbowkit';
+import {ThemeProvider} from './theme/theme-provider';
 import {wagmiConfig} from '~/lib/wagmi-config';
 import '@rainbow-me/rainbowkit/styles.css';
 
@@ -14,7 +15,16 @@ export function Providers({children}: {children: React.ReactNode}) {
     <>
       <WagmiProvider config={wagmiConfig}>
         <QueryClientProvider client={queryClient}>
-          <RainbowKitProvider>{children}</RainbowKitProvider>
+          <RainbowKitProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          </RainbowKitProvider>
         </QueryClientProvider>
       </WagmiProvider>
     </>
